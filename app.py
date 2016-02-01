c9133425bd4c559886b52966927b25b158b91397
chapman3 <chapman3@uoregon.edu>
2016-02-01 05:32:03 -0800

    debugging

--- app.py
+++ app.py
@@ -71,7 +71,9 @@ def calc_times():
   km = request.args.get('km', 0, type=int)
   start_time = request.args.get('start_tm', 0, type=str)
   start_date = request.args.get('start_dt', 0, type=str)
+  print(km,start_date,start_time)
   times = brevet_calc.get_times(km)
+
   new_open_close = brevet_calc.handle_new_time(start_date,start_time,times[0],times[1])
   rslt = {"new_open": new_open_close[0], "new_close": new_open_close[1]}
   return jsonify(result=rslt)
